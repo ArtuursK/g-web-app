2da1d006bdc9ff9ac25ecb44d5687ac91aa132f7
IZV\adm_ak <arturs@arturs.com>
2024-03-21 10:32:05 +0200

    Initial commit

---
+++ src/main/java/com/gwap/GwapApplication.java
@@ -0,0 +1,13 @@
+package com.gwap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwapApplication.class, args);
+	}
+
+}
