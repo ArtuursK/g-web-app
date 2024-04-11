7317534535276e350168e6228c77ed75a46210d0
IZV\adm_ak <arturs@arturs.com>
2024-04-11 09:55:42 +0300

    Add initial comm

---
+++ src/main/java/com/gwap/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.gwap.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/")
+    public String getRoot() {
+        //TODO: get posts from db
+        //TODO: add post list to page
+        return "index";
+    }
+
+}
